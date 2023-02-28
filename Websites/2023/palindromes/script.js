58f620777d582ddaa1ecd4262c807b125b23476e
PetrBlue <petrpm8@email.cz>
2023-02-28 21:39:30 +0100

    Auto commit.

--- Websites/2023/palindromes/script.js
+++ Websites/2023/palindromes/script.js
@@ -1,12 +1,13 @@
-function Palindrome(){
-    if(IsItPalindrome()){
+function Palindrome() {
+    if (IsItPalindrome()) {
         document.getElementById('text').textContent = "It is a palindrome!";
-    }else{
+    } else {
         document.getElementById('text').textContent = "It isn't a palindrome!";
     }
 }
-function IsItPalindrome(){
+function IsItPalindrome() {
     let value = document.getElementById('value').value;
+    //Reverses a string
     let valueReversed = value.split("").reverse().join("");
     return value == valueReversed;
 }
